fb7c176f40cd5082c3b56a6e154b8d06d4f4b11c
FirdausDY <firdausdwiyanto16@gmail.com>
2021-07-22 20:59:11 -1200

    Final sudah jadi

---
+++ app/src/main/java/com/firdaus/pengingattugas/adapter/CustomListOpsiAdapter.java
@@ -0,0 +1,52 @@
+package com.firdaus.pengingattugas.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.firdaus.pengingattugas.R;
+
+import java.util.List;
+
+
+public class CustomListOpsiAdapter extends ArrayAdapter<ListOpsiData> {
+    private Context context;
+    private List<ListOpsiData> listOpsiData;
+
+    public CustomListOpsiAdapter(@NonNull Context context, int resource, List<ListOpsiData> listOpsiData) {
+        super(context, resource, listOpsiData);
+        this.context = context;
+        this.listOpsiData = listOpsiData;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return myCustomListOpsiAdapter(position, convertView, parent);
+    }
+
+    @Override
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return myCustomListOpsiAdapter(position, convertView, parent);
+    }
+
+    private View myCustomListOpsiAdapter(int position, @Nullable View myView, @Nullable ViewGroup parent){
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View costumView = layoutInflater.inflate(R.layout.costum_list, parent, false);
+
+        TextView opsiNama = (TextView) costumView.findViewById(R.id.opsiNama);
+        ImageView opsiGambar = (ImageView) costumView.findViewById(R.id.gambarOpsi);
+
+        opsiNama.setText(listOpsiData.get(position).getIconNama());
+        opsiGambar.setImageResource(listOpsiData.get(position).getIcon());
+
+        return costumView;
+    }
+}
---
+++ app/src/main/java/com/firdaus/pengingattugas/adapter/ListOpsiData.java
@@ -0,0 +1,20 @@
+package com.firdaus.pengingattugas.adapter;
+
+
+public class ListOpsiData {
+    private int icon;
+    private String iconNama;
+
+    public ListOpsiData(int icon, String iconNama) {
+        this.icon = icon;
+        this.iconNama = iconNama;
+    }
+
+    public int getIcon() {
+        return icon;
+    }
+
+    public String getIconNama() {
+        return iconNama;
+    }
+}
